443879059978ba102a025c37b8f8e9d4d18283bf
Dulaj <dwalpalage@gmail.com>
2026-03-12 12:48:47 +0530

    u

--- contact.php
+++ contact.php
@@ -563,7 +563,7 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
 
 </div>
 
-<!-- FOOER sss-->
+<!-- FOOERhhh sss-->
 <footer>
     <p>&copy; 2026 <span>Velvet Vogue</span> &mdash; All Rights Reserved</p>
 </footer>
