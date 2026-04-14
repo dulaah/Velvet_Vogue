ee27c3478bb597fdb6a62a6b0e7c42f01e97430a
Dulaj <dwalpalage@gmail.com>
2026-04-14 19:58:37 +0530

    u

--- contact.php
+++ contact.php
@@ -325,7 +325,7 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
             line-height: 1.7;
         }
 
-        /* ─── FORM FIELDS ────────────────────────── */
+        /* ─── FORM FIELDS ──────────────────────────   dsss*/
         .form-row {
             display: grid;
             grid-template-columns: 1fr 1fr;
